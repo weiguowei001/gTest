66358e045f8296cfce577ee3cfa74138697d48eb
魏国威 <weiguowei@zqykj.com>
2020-07-27 16:51:12 +0800

    modify LocalDebuggerCommandArguments and add function subtract in main.cpp

--- MyTest/src/main.cpp
+++ MyTest/src/main.cpp
@@ -3,8 +3,13 @@
 int add(int a, int b) {
 	return a + b;
 }
+
+int subtract(int a, int b) {
+	return a - b;
+}
 TEST(testCase, test0) {
-	EXPECT_EQ(add(2, 3), 7);
+	EXPECT_EQ(add(2, 3), 5);
+	EXPECT_EQ(subtract(10, 4), 7);
 }
 int main(int argc, char **argv) {
 	testing::InitGoogleTest(&argc, argv);
